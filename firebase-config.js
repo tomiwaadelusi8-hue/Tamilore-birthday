55a4537f6f1bcacd5614eda415ca90c8f37bf440
tomiwaadelusi8-hue <tomiwaadelusi8@gmail.com>
2026-03-24 12:28:42 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBH8Z73-oIT4MSneaWy0TkfhY8Z-gIBqA8",
+  authDomain: "birthd-1fe1d.firebaseapp.com",
+  projectId: "birthd-1fe1d",
+  storageBucket: "birthd-1fe1d.firebasestorage.app",
+  messagingSenderId: "684531370376",
+  appId: "1:684531370376:web:c73e9d5177ecaa99b5523d"
+};
